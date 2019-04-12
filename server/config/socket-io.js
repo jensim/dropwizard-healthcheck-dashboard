9df36aef7e37aca2ddeac58e8d7107fa017f5b16
Jens Brimfors <jens.brimfors@nordnet.se>
2019-04-12 10:50:20 +0200

    Lint fixed file

--- server/config/socket-io.js
+++ server/config/socket-io.js
@@ -3,20 +3,21 @@ const Host = require("../models/host.model");
 module.exports = function configure(server, app) {
   let io = require("socket.io")(server);
   app.set("io", io);
+  global.io = io;
 
-  setInterval(function () {
-    io.emit('time', new Date);
+  setInterval(function() {
+    io.emit("time", new Date());
   }, 5000);
 
-  io.on('connection', function (socket) {
-    socket.emit('chat message', 'hello and welcome');
-    socket.on('chat message', function (data) {
-      io.emit('chat message', data)
+  io.on("connection", function(socket) {
+    socket.emit("chat message", "hello and welcome");
+    socket.on("chat message", function(data) {
+      io.emit("chat message", data);
     });
-    socket.on('refresh hosts', () => {
+    socket.on("refresh hosts", () => {
       (async () => {
         for await (const host of Host.find()) {
-          socket.emit('refresh hosts', host);
+          socket.emit("refresh hosts", host);
         }
       })();
     });
