95cbdd4ba15e4999782575bff26e48359777459f
Jens Brimfors <jens.brimfors@nordnet.se>
2019-04-19 08:03:17 +0200

    Print server started stare a bit more visibly

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@
 const config = require("./config/config");
 const app = require("./config/express");
 require("./config/mongoose");
+const chalk = require("chalk");
 
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   let server = app.listen(config.port, () => {
-    console.info(`server started on port ${config.port} (${config.env})`);
+    console.info(chalk.green.underline.bold(`Server started on port ${config.port} (${config.env})`));
   });
   require("./config/socket-io")(server, app);
 }
